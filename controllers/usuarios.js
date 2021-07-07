0d8da9ef0afd359c2a9d5bd1bddbc5354f855a39
brayan walteros <brayanwalteros1@gmail.com>
2021-07-07 08:02:03 -0500

    primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,60 @@
+const { response, request} = require("express");
+
+
+
+const usuariosGet = (req = request, res = response) => {
+
+  const {q,nombre = "no name",apikey,page = 1,limit } = req.query;
+
+    res.json({
+        
+        msg: "get API-usuariosGet",
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+  }
+
+  const usuariosPut = (req, res = response) => {
+
+    const id = req.params.id;
+             res.json({
+        
+        msg: "put API-usuariosPut",
+        id
+    });
+  };
+
+  const usuariosPost = (req, res = response) => {
+
+    const {nombre, edad,apellido} = req.body;
+
+    res.json({
+        
+        msg: "post API-usuariosPost",
+        nombre,
+        edad,
+        apellido
+    });
+  };
+  const usuariosDelete = (req, res = response) => {
+                res.json({
+        
+        msg: "delete API-usuariosDelete"
+    });
+  };
+
+  const usuariosPatch = (req, res = response) => {
+    res.json({
+        
+        msg: "patch API-usuariosPatch"
+    });
+  }
+
+
+
+  module.exports = {
+      usuariosGet,usuariosPut,usuariosPost,usuariosDelete,usuariosPatch
+  }
